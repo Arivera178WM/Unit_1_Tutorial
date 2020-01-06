f610a5be33b1b86014606a47abc000b0cba1ebf5
Abraham <arivera178@west-mec.org>
2020-01-06 15:27:29 -0700

    Complete with Day 1.6.20

---
+++ tny_script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Abraham Rivera Garcia
+   Date:   Jan6, 2020
+
+*/
+
+MediaStreamTrackAudioSourceNode();
+document.getElementsById("datenow");
+document.getSelection.name("Jeff");
